a1d298fd34a9b699ba776c572ec761ad551f57a8
Vitalii Chekushkin <89994306+Vitstark@users.noreply.github.com>
2022-02-19 23:37:16 +0300

    Stack commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import homework1.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hmm...");
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 1; i++) {
+            stack.push(i);
+        }
+        System.out.println(stack);
+        System.out.println(stack.peek() + stack.pop() + " " + stack.isEmpty());
+        System.out.println(stack);
     }
 }
